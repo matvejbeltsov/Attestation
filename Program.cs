72e1d1821af840244c3ccbe3cabfe438ca23e44f
MatvejBeltsov <matvej.beltsov@gmail.com>
2023-12-03 12:42:11 +0300

    Создание метода inizialisation_array

--- Program.cs
+++ Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.XPath;
+using Microsoft.Win32.SafeHandles;
+
+namespace Attestation
+{
+    internal class Program
+    {
+        public static string[] inizialisation_array(int length_of_array){
+            string[] array = new string[length_of_array];
+            Console.WriteLine("Введите слова, которые вы будете хранить в данном массиве");
+            for (int i = 0; i < array.Length; i++){
+                array[i] = Console.ReadLine();
+            }
+            return array;
+
+        }
+
+        
+        
+       
+        static void Main(string[] args)
+        {
+            Console.Write("Укажите длинну массива: ");
+            int length_of_array = Convert.ToInt32(Console.ReadLine());
+            string[] array = inizialisation_array(length_of_array);
+
+            for (int i = 0; i < array.Length; i++){
+                Console.Write(array[i] + " ");
+            }
+        }
+    }
+}
