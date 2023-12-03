937dbbaab34b9917b4e298a1b095edf411df8a27
MatvejBeltsov <matvej.beltsov@gmail.com>
2023-12-03 14:23:08 +0300

    Добавление метода solving_the_problem, который решает нашу задачу

--- Program.cs
+++ Program.cs
@@ -28,6 +28,24 @@ namespace Attestation
             Console.Write($"\"{array[array.Length - 1]}\"]");
         }
 
+        public static string[] solving_the_problem(string[] array){
+            int count = 0;
+            for (int i = 0; i < array.Length; i++){
+                if(array[i].Length <= 3){
+                    count++;
+                }
+            }
+            string[] new_array = new string[count];
+            int index = 0;
+            for(int i = 0; i < array.Length; i++){
+                if(array[i].Length <= 3){
+                    new_array[index] = array[i];
+                    index++;
+                }
+            }
+            return new_array;
+        }
+
         
         
        
@@ -36,7 +54,11 @@ namespace Attestation
             Console.Write("Укажите длинну массива: ");
             int length_of_array = Convert.ToInt32(Console.ReadLine());
             string[] array = inizialisation_array(length_of_array);
+            Console.WriteLine("Изначальный массив: ");
             print_array(array);
+            string[] new_array = solving_the_problem(array);
+            Console.WriteLine("\nРезультат выполнения программы: ");
+            print_array(new_array);
         }
     }
 }
